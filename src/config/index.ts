7b42c4cf2ce91c6191614e6d2eb3c7a24e297521
Abdullah Al Jubair <jubair2810@gmail.com>
2025-12-16 18:29:54 +0600

    added:config file

---
+++ src/config/index.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  port: process.env.PORT,
+  db_url: process.env.DB_URL,
+  node_env: process.env.NODE_ENV,
+  jwt: {
+    access_token: process.env.ACCESS_TOKEN,
+  },
+};
