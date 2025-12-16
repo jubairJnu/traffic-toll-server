2202f1cef9ba1400cb586c900f3d2e9fc6f3ebc4
Abdullah Al Jubair <jubair2810@gmail.com>
2025-12-16 19:31:19 +0600

    added:vehicle schema

---
+++ src/vehicles/schemas/vehicles.schema.ts
@@ -0,0 +1,34 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+
+export type vehicleDocument = Vehicle & Document;
+
+@Schema({ timestamps: true })
+export class Vehicle {
+  @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  ownerId: mongoose.Types.ObjectId;
+
+  @Prop({ required: true, unique: true, trim: true })
+  plateNumber: string;
+
+  @Prop({
+    required: true,
+    enum: ['motorcycle', 'car', 'bus', 'truck', 'three wheel', 'pick-up'],
+    index: true,
+  })
+  vehicleType: string;
+
+  @Prop({ default: 0 })
+  balance: number;
+
+  @Prop()
+  autoChargeEnable: boolean;
+
+  @Prop({ default: 0 })
+  totalDue: number;
+
+  @Prop()
+  lastPaidAt: Date;
+}
+
+export const vehicleSchema = SchemaFactory.createForClass(Vehicle);
