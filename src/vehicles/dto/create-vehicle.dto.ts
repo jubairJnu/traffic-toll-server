1e92673d9a75c6c6800df71af4da12fa543b858b
Abdullah Al Jubair <jubair2810@gmail.com>
2025-12-16 22:21:56 +0600

    added: vehiccle services

--- src/vehicles/dto/create-vehicle.dto.ts
+++ src/vehicles/dto/create-vehicle.dto.ts
@@ -1,1 +1,26 @@
-export class CreateVehicleDto {}
+import {
+  IsBoolean,
+  IsEnum,
+  IsMongoId,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateVehicleDto {
+  @IsMongoId()
+  @IsNotEmpty()
+  ownerId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  plateNumber: string;
+
+  @IsEnum(['motorcycle', 'car', 'bus', 'truck', 'three wheel', 'pick-up'])
+  vehicleType: string;
+
+  @IsOptional()
+  @IsBoolean()
+  autoChargeEnable?: boolean;
+  balance: number;
+}
--- src/vehicles/vehicles.service.ts
+++ src/vehicles/vehicles.service.ts
@@ -1,26 +1,61 @@
 import { Injectable } from '@nestjs/common';
 import { CreateVehicleDto } from './dto/create-vehicle.dto';
 import { UpdateVehicleDto } from './dto/update-vehicle.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { vehicleDocument } from './schemas/vehicles.schema';
+import QueryBuilder from 'src/builder/QueryBuilder';
+import { vehicleSearchableFields } from './vehicle.constance';
+import { IPaginateMeta } from 'src/interface';
 
 @Injectable()
 export class VehiclesService {
-  create(createVehicleDto: CreateVehicleDto) {
-    return 'This action adds a new vehicle';
+  constructor(
+    @InjectModel('Vehicle') private vehicleModel: Model<vehicleDocument>,
+  ) {}
+
+  async create(createVehicleDto: CreateVehicleDto, ownerId: Types.ObjectId) {
+    const payload = {
+      ...createVehicleDto,
+      ownerId,
+    };
+
+    return await this.vehicleModel.create(payload);
   }
 
-  findAll() {
-    return `This action returns all vehicles`;
+  async findAll(
+    query: Record<string, any>,
+  ): Promise<{ result: any[]; meta: IPaginateMeta }> {
+    const resultQuery = new QueryBuilder(
+      this.vehicleModel.find().populate('ownerId', 'name email phone'),
+      query,
+    )
+      .search(vehicleSearchableFields)
+      .filter()
+      .sort()
+      .paginate();
+
+    const result = await resultQuery.modelQuery;
+    const meta = await resultQuery.countTotal();
+    return {
+      meta,
+      result,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} vehicle`;
+  async findOne(id: string) {
+    return await this.vehicleModel
+      .findById(id)
+      .populate('ownerId', 'name email phone');
   }
 
-  update(id: number, updateVehicleDto: UpdateVehicleDto) {
-    return `This action updates a #${id} vehicle`;
+  async update(id: string, updateVehicleDto: UpdateVehicleDto) {
+    return await this.vehicleModel.findByIdAndUpdate(id, updateVehicleDto, {
+      new: true,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} vehicle`;
+  async remove(id: string) {
+    return await this.vehicleModel.findByIdAndDelete(id);
   }
 }
