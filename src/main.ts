6bc27d47f2a14d4e8aae144b38b912c996dbf859
Abdullah Al Jubair <jubair2810@gmail.com>
2025-12-16 18:26:10 +0600

    inti porject

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const port = 3001;
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port);
+  console.log(`app is listening on:http://localhost:${port} `);
+}
+bootstrap();
