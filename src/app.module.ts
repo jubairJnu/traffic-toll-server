aa2f389dd54fe25d8ed3b2bc2dd3e27a920bb454
Abdullah Al Jubair <jubair2810@gmail.com>
2025-12-16 20:11:04 +0600

    added: mongoose connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -9,9 +10,46 @@ import { TollplazaModule } from './tollplaza/tollplaza.module';
 import { TolleventsModule } from './tollevents/tollevents.module';
 import { TransactionsModule } from './transactions/transactions.module';
 import { NotificationsModule } from './notifications/notifications.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Module({
-  imports: [UsersModule, RoadsegmentsModule, RoadestimateModule, VehiclesModule, TollplazaModule, TolleventsModule, TransactionsModule, NotificationsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('DB_URL'),
+        connectionFactory: (connection: Connection) => {
+          connection.on('connected', () => {
+            console.log('🟢 MongoDB connected');
+          });
+
+          connection.on('error', (error) => {
+            console.error('🔴 MongoDB connection failed:', error);
+          });
+
+          connection.on('disconnected', () => {
+            console.log('🟡 MongoDB disconnected');
+          });
+
+          return connection;
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    RoadsegmentsModule,
+    RoadestimateModule,
+    VehiclesModule,
+    TollplazaModule,
+    TolleventsModule,
+    TransactionsModule,
+    NotificationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
