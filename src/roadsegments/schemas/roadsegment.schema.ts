e096b2d190226215cb77fedb708a4fd1f2450ba8
Abdullah Al Jubair <jubair2810@gmail.com>
2025-12-16 19:58:53 +0600

    added: road segments

---
+++ src/roadsegments/schemas/roadsegment.schema.ts
@@ -0,0 +1,28 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+
+export type RoadDocument = RoadSegment & Document;
+
+@Schema({ timestamps: true })
+export class RoadSegment {
+  @Prop({ required: true, trim: true })
+  fromCity: string;
+
+  @Prop({ required: true, tirm: true })
+  toCity: string;
+
+  @Prop({ required: true })
+  distanceKm: number;
+
+  @Prop({
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'TollPlaza',
+  })
+  plazes: mongoose.Schema.Types.ObjectId[];
+}
+
+export const RoadSegmentSchema = SchemaFactory.createForClass(RoadSegment);
+
+RoadSegmentSchema.index({ fromCity: 1 });
+RoadSegmentSchema.index({ toCity: 1 });
