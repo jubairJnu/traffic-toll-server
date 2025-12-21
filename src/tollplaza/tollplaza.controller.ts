adb22a011995a037471efcaa38e5cd770425c68d
Abdullah Al Jubair <jubair2810@gmail.com>
2025-12-21 22:29:06 +0600

    refacotre and fix tolplaza controller

--- src/tollplaza/tollplaza.controller.ts
+++ src/tollplaza/tollplaza.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Req,
+} from '@nestjs/common';
 import { TollplazaService } from './tollplaza.service';
 import { CreateTollplazaDto } from './dto/create-tollplaza.dto';
 import { UpdateTollplazaDto } from './dto/update-tollplaza.dto';
@@ -13,22 +22,25 @@ export class TollplazaController {
   }
 
   @Get()
-  findAll() {
-    return this.tollplazaService.findAll();
+  findAll(@Req() req) {
+    return this.tollplazaService.findAll(req.query);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.tollplazaService.findOne(+id);
+    return this.tollplazaService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTollplazaDto: UpdateTollplazaDto) {
-    return this.tollplazaService.update(+id, updateTollplazaDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateTollplazaDto: UpdateTollplazaDto,
+  ) {
+    return this.tollplazaService.update(id, updateTollplazaDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.tollplazaService.remove(+id);
+    return this.tollplazaService.remove(id);
   }
 }
