d62bc9150194c39dc0b3dbd530d72fa9e454d4da
Abdullah Al Jubair <jubair2810@gmail.com>
2025-12-27 21:08:29 +0600

    create tollplaza dto

--- src/tollplaza/dto/create-tollplaza.dto.ts
+++ src/tollplaza/dto/create-tollplaza.dto.ts
@@ -1,1 +1,45 @@
-export class CreateTollplazaDto {}
+import {
+  IsArray,
+  IsIn,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+export class LocationDto {
+  @IsIn(['Point'])
+  type: 'Point';
+
+  @IsArray()
+  @IsNumber({}, { each: true })
+  coordinates: [number, number];
+}
+
+export class TollRateDto {
+  @IsString()
+  @IsNotEmpty()
+  vehicleType: string;
+
+  @IsNumber()
+  amount: number;
+}
+
+export class CreateTollplazaDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  city: string;
+
+  @ValidateNested()
+  @Type(() => LocationDto)
+  location: LocationDto;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => TollRateDto)
+  rates: TollRateDto[];
+}
