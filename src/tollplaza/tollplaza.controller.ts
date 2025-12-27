32dbcf3a53c1119437de0092bd58b4f16defc7ca
Abdullah Al Jubair <jubair2810@gmail.com>
2025-12-27 21:15:17 +0600

    update toll plaza

--- src/tollplaza/tollplaza.controller.ts
+++ src/tollplaza/tollplaza.controller.ts
@@ -11,36 +11,67 @@ import {
 import { TollplazaService } from './tollplaza.service';
 import { CreateTollplazaDto } from './dto/create-tollplaza.dto';
 import { UpdateTollplazaDto } from './dto/update-tollplaza.dto';
+import { sendResponse } from 'src/utils/sendResponse';
 
 @Controller('tollplaza')
 export class TollplazaController {
   constructor(private readonly tollplazaService: TollplazaService) {}
 
   @Post()
-  create(@Body() createTollplazaDto: CreateTollplazaDto) {
-    return this.tollplazaService.create(createTollplazaDto);
+  async create(@Body() createTollplazaDto: CreateTollplazaDto) {
+    const result = this.tollplazaService.create(createTollplazaDto);
+    return sendResponse({
+      success: true,
+      statusCode: 200,
+      message: 'created tollplaza',
+      data: result,
+    });
   }
 
   @Get()
-  findAll(@Req() req) {
-    return this.tollplazaService.findAll(req.query);
+  async findAll(@Req() req) {
+    const result = await this.tollplazaService.findAll(req.query);
+    return sendResponse({
+      success: true,
+      statusCode: 200,
+      message: 'retrieved tollplaza',
+      data: result,
+    });
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.tollplazaService.findOne(id);
+  async findOne(@Param('id') id: string) {
+    const result = await this.tollplazaService.findOne(id);
+    return sendResponse({
+      success: true,
+      statusCode: 200,
+      message: 'retrieved tollplaza',
+      data: result,
+    });
   }
 
   @Patch(':id')
-  update(
+  async update(
     @Param('id') id: string,
     @Body() updateTollplazaDto: UpdateTollplazaDto,
   ) {
-    return this.tollplazaService.update(id, updateTollplazaDto);
+    const result = await this.tollplazaService.update(id, updateTollplazaDto);
+    return sendResponse({
+      success: true,
+      statusCode: 200,
+      message: 'updated tollplaza',
+      data: result,
+    });
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.tollplazaService.remove(id);
+  async remove(@Param('id') id: string) {
+    const result = await this.tollplazaService.remove(id);
+    return sendResponse({
+      success: true,
+      statusCode: 200,
+      message: 'deleted tollplaza',
+      data: result,
+    });
   }
 }
